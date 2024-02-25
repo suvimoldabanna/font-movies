caf32d276f49e313243b1069b71aade0b40ee08f
suvimoldabanna <152445319+suvimoldabanna@users.noreply.github.com>
2024-02-26 05:01:09 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -51,21 +51,23 @@ const index = ({movies,categories,types}) => {
           ml: { sm: `${drawerWidth}px` },
         }}
       >
-        <Toolbar className='flex justify-between'>
-          <Typography variant="h6" noWrap component="div">
-            Responsive drawer
-          </Typography>
+        <div className='Navbar'>
+      <Toolbar className='flex justify-between '>
+        <Typography variant="h6" noWrap component="div">
+        
+        </Typography>
 
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: 'none' } }}
-          >
-           <MenuIcon />
-          </IconButton>
-        </Toolbar>
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          edge="start"
+          onClick={handleDrawerToggle}
+          sx={{ mr: 2, display: { sm: 'none' } }}
+        >
+         <MenuIcon />
+        </IconButton>
+      </Toolbar>
+      </div>
       </AppBar>
       <Box
         component="nav"
