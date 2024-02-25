7c06bb1aa715a1e79736e0a2592f753c37c65e82
suvimoldabanna <152445319+suvimoldabanna@users.noreply.github.com>
2024-02-26 05:01:50 +0700

    Update [category].js

--- pages/category/[category].js
+++ pages/category/[category].js
@@ -60,21 +60,23 @@ const category = ({movies,categories,types,categorie}) => {
           ml: { sm: `${drawerWidth}px` },
         }}
       >
-        <Toolbar className='flex justify-between'>
-          <Typography variant="h6" noWrap component="div">
-            Responsive drawer
-          </Typography>
+         <div className='Navbar'>
+      <Toolbar className='flex justify-between '>
+        <Typography variant="h6" noWrap component="div">
+        
+        </Typography>
 
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: 'none' } }}
-          >
-           <MenuIcon />
-          </IconButton>
-        </Toolbar>
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          edge="start"
+          onClick={handleDrawerToggle}
+          sx={{ mr: 2, display: { sm: 'none' } }}
+        >
+         <MenuIcon />
+        </IconButton>
+      </Toolbar>
+      </div>
       </AppBar>
       <Box
         component="nav"
